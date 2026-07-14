d809c9f3b21acd822d5f1a562aec45c0db61807e
Basma <bsmtbdalawl@gmail.com>
2026-07-14 05:21:40 +0300

    second commit

--- js/login.js
+++ js/login.js
@@ -4,8 +4,7 @@ let email = document.querySelector("#email")
 let password = document.querySelector("#password")
 let loginBtn = document.querySelector("#loginBtn")
 
-// let localEmail = localStorage.getItem(email.value)
-// let localPassword = localStorage.getItem(password.value)
+
 
 loginBtn.addEventListener('click', function (e) {
     e.preventDefault()
@@ -13,13 +12,7 @@ loginBtn.addEventListener('click', function (e) {
         alert("please, fill in all fields")
     }
     else {
-        // let first = firstName.value
-        // let last = lastName.value
-        // let Email = email.value
-        // let Password = password.value
         let users = JSON.parse(localStorage.getItem("users")) || []
-        // let userInfo = { Password, first, last }
-
         let isvalid1 = users.some(item => item.Email == email.value && item.userInfo.Password == password.value)
         let findUser = users.find(item => item.Email == email.value && item.userInfo.Password == password.value)
 
